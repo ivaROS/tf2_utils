342b68f6fad3b8f80fa16b0a776894ac33945ecb
Justin Smith <jssmith@gatech.edu>
2021-10-29 13:41:59 -0400

    Fill in TransformManager

--- include/tf2_utils/transform_manager.h
+++ include/tf2_utils/transform_manager.h
@@ -6,26 +6,61 @@
 
 namespace tf2_utils
 {
-
-  class TransformManager :
+  
+  class TransformManager
   {
     std::shared_ptr<tf2_ros::Buffer> buffer_;
-    std::shared_ptr<tf2_ros::Listener> listener_;
-  
-public:
-  
-    TransformManager(double buffer_size=10) :
-      buffer_(std::make_shared<tf2_ros::Buffer>(buffer_size))
-      listener_(std::make_shared<tf2_ros::TransformListener>(*buffer))
+    std::shared_ptr<tf2_ros::TransformListener> listener_;
+    
+    
+    void initialize(ros::NodeHandle nh=ros::NodeHandle(), ros::Duration buffer_size=ros::Duration(tf2::BufferCore::DEFAULT_CACHE_TIME))
+    {
+      buffer_ = std::make_shared<tf2_ros::Buffer>(buffer_size);
+      listener_ = std::make_shared<tf2_ros::TransformListener>(*buffer_, nh);
+    }
+    
+  public:
+    
+    
+    TransformManager(ros::Duration buffer_size)
+    {
+      ros::NodeHandle nh;
+      initialize(nh, buffer_size);
+    }
+    
+    TransformManager(bool create=false)
+    {
+      if(create)
+      {
+        initialize();
+      }
+    }
+    
+    TransformManager(TransformManager tfm, ros::NodeHandle nh, ros::Duration buffer_size=ros::Duration(tf2::BufferCore::DEFAULT_CACHE_TIME))
+    {
+      if(!tfm.buffer_)
+      {
+        initialize(nh, buffer_size);
+      }
+      else
+      {
+        buffer_ = tfm.buffer_;
+        listener_ = tfm.listener_;
+      }
+    }
+    
+    TransformManager(std::shared_ptr<tf2_ros::Buffer> buffer, std::shared_ptr<tf2_ros::TransformListener> listener):
+      buffer_(buffer),
+      listener_(listener)
     {}
     
-    std::shared_ptr<tf2_ros::Buffer> getBuffer()
+    std::shared_ptr<tf2_ros::Buffer> getBuffer() const
     {
       return buffer_;
     }
     
   };
-
+  
 } //end namespace tf2_utils
 
 #endif //TF2_UTILS_TRANSFORM_MANAGER_H
